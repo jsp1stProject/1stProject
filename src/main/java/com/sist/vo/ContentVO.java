c3e845f24fdf5629b77354fd1c42d806e240e553
hare211 <blackhole2748@gmail.com>
2025-03-06 17:11:59 +0900

    session 생성 클래스, config alias, contentvo 추가

---
+++ src/main/java/com/sist/vo/ContentVO.java
@@ -0,0 +1,13 @@
+package com.sist.vo;
+
+import lombok.Data;
+
+@Data
+public class ContentVO {
+	private int contentId;
+	private String addr1, addr2, zipcode, firstImage, firstImage2, tel, cat1, cat2, cat3;
+	private int contentTypeId, sigunguCode, areaCode;
+	private double mapX, mapY;
+	
+	HotelVO hvo = new HotelVO();
+}
