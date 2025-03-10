6a6c29aead42516c3bc58ece3d5dccce787b2f29
hare211 <blackhole2748@gmail.com>
2025-03-10 10:00:19 +0900

    김기현 - Config.xml mapper 추가

--- src/main/java/com/sist/vo/ContentVO.java
+++ src/main/java/com/sist/vo/ContentVO.java
@@ -7,6 +7,6 @@ public class ContentVO {
 	private int content_id;
 	private String title, addr1, addr2, zipcode, first_image, first_image2, tel, cat1, cat2, cat3;
 	private int content_type_id, sigungucode, areacode;
-	private double mapx, mapy;
+	private double mapx, mapy; 
 	
 }
