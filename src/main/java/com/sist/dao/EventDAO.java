1d4155ded6093141c59b2fb9f1b05eba8f9a5ad7
narin45446 <narin45446@gmail.com>
2025-03-13 16:25:56 +0900

    김나린 - 행사 지역보기 추가, 쿼리 수정

--- src/main/java/com/sist/dao/EventDAO.java
+++ src/main/java/com/sist/dao/EventDAO.java
@@ -5,102 +5,31 @@ import com.sist.vo.EventVO;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EventDAO {
 	private static SqlSessionFactory ssf;
-	
+	public static LinkedHashMap catemap = new LinkedHashMap();
+
 	static {
 		ssf = CreateSqlSessionFactory.getSsf();
-	}
-	
-	public static List<EventVO> mainEventList() {
-		SqlSession session = null;
-		List<EventVO> list = null;
-		try{
-			session = ssf.openSession();
-			list = session.selectList("mainEventList");
-			for(EventVO vo:list){
-				String cat3=vo.getCvo().getCat3().substring(5,7);
-				switch (cat3){
-					case "01":vo.setDbcate("전통공연");break;
-					case "02":vo.setDbcate("연극");break;
-					case "03":vo.setDbcate("뮤지컬");break;
-					case "04":vo.setDbcate("오페라");break;
-					case "05":vo.setDbcate("전시회");break;
-					case "06":vo.setDbcate("박람회");break;
-					case "08":vo.setDbcate("무용");break;
-					case "09":vo.setDbcate("클래식");break;
-					case "10":vo.setDbcate("콘서트");break;
-					case "11":vo.setDbcate("영화");break;
-					case "12":vo.setDbcate("스포츠");break;
-					case "13":vo.setDbcate("기타행사");break;
-				}
-			}
-		} catch(Exception e){
-			e.printStackTrace();
-		} finally {
-			if(session != null){
-				session.close();
-			}
-		}
-		return list;
+		catemap.put("A02080100", "전통공연");
+		catemap.put("A02080200", "연극");
+		catemap.put("A02080300", "뮤지컬");
+		catemap.put("A02080400", "오페라");
+		catemap.put("A02080500", "전시회");
+		catemap.put("A02080600", "박람회");
+		catemap.put("A02080800", "무용");
+		catemap.put("A02080900", "클래식");
+		catemap.put("A02081000", "콘서트");
+		catemap.put("A02081100", "영화");
+		catemap.put("A02081200", "스포츠");
+		catemap.put("A02081300", "기타행사");
 	}
 
-	public static List<EventVO> mainFesList() {
-		SqlSession session = null;
-		List<EventVO> list = new ArrayList<EventVO>();
-		try{
-			session = ssf.openSession();
-			list= session.selectList("mainFesList");
-			for(EventVO vo:list){
-				int areaCode = vo.getCvo().getAreacode();
-				System.out.println(areaCode);
-				switch (areaCode){
-					case 1:vo.setDbarea("서울");break; //146
-					case 2:vo.setDbarea("인천");break; //22
-					case 3:vo.setDbarea("대전");break; //6
-					case 4:vo.setDbarea("대구");break;
-					case 5:vo.setDbarea("광주");break;
-					case 6:vo.setDbarea("부산");break;
-					case 7:vo.setDbarea("울산");break;
-					case 8:vo.setDbarea("세종");break;
-					case 31:vo.setDbarea("경기");break;
-					case 32:vo.setDbarea("강원");break;
-					case 33:vo.setDbarea("충북");break;
-					case 34:vo.setDbarea("충남");break;
-					case 35:vo.setDbarea("경북");break;
-					case 36:vo.setDbarea("경남");break;
-					case 37:vo.setDbarea("전북");break;
-					case 38:vo.setDbarea("전남");break;
-					case 39:vo.setDbarea("제주");break;
-				}
-			}
-		} catch(Exception e){
-			e.printStackTrace();
-		} finally {
-			if(session != null){
-				session.close();
-			}
-		}
-		return list;
-	}
-	public static List<EventVO> eventAreaList(HashMap map){
-		SqlSession session = ssf.openSession();
-		List<EventVO> list= session.selectList("eventAreaList", map);
-		session.close();
-		return list;
-	}
-
-	public static List<EventVO> eventSearchList(HashMap map){
-		SqlSession session = ssf.openSession();
-		List<EventVO> list= session.selectList("eventSearchList", map);
-		
+	public static void categorySet(List<EventVO> list){
 		for(EventVO vo:list){
 			String cat3=vo.getCvo().getCat3().substring(5,7);
 			switch (cat3){
@@ -117,8 +46,79 @@ public class EventDAO {
 				case "12":vo.setDbcate("스포츠");break;
 				case "13":vo.setDbcate("기타행사");break;
 			}
-
 		}
+	}
+	public static void areaSet(List<EventVO> list){
+		for(EventVO vo:list){
+			int areaCode = vo.getCvo().getAreacode();
+			System.out.println(areaCode);
+			switch (areaCode){
+				case 1:vo.setDbarea("서울");break;
+				case 2:vo.setDbarea("인천");break;
+				case 3:vo.setDbarea("대전");break;
+				case 4:vo.setDbarea("대구");break;
+				case 5:vo.setDbarea("광주");break;
+				case 6:vo.setDbarea("부산");break;
+				case 7:vo.setDbarea("울산");break;
+				case 8:vo.setDbarea("세종");break;
+				case 31:vo.setDbarea("경기");break;
+				case 32:vo.setDbarea("강원");break;
+				case 33:vo.setDbarea("충북");break;
+				case 34:vo.setDbarea("충남");break;
+				case 35:vo.setDbarea("경북");break;
+				case 36:vo.setDbarea("경남");break;
+				case 37:vo.setDbarea("전북");break;
+				case 38:vo.setDbarea("전남");break;
+				case 39:vo.setDbarea("제주");break;
+			}
+		}
+	}
+	
+	public static List<EventVO> mainEventList() {
+		SqlSession session = null;
+		List<EventVO> list = null;
+		try{
+			session = ssf.openSession();
+			list = session.selectList("mainEventList");
+			categorySet(list);
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally {
+			if(session != null){
+				session.close();
+			}
+		}
+		return list;
+	}
+
+	public static List<EventVO> mainFesList() {
+		SqlSession session = null;
+		List<EventVO> list = new ArrayList<EventVO>();
+		try{
+			session = ssf.openSession();
+			list= session.selectList("mainFesList");
+			areaSet(list);
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally {
+			if(session != null){
+				session.close();
+			}
+		}
+		return list;
+	}
+	public static List<EventVO> eventAreaList(HashMap map){
+		SqlSession session = ssf.openSession();
+		List<EventVO> list= session.selectList("eventAreaList", map);
+		categorySet(list);
+		session.close();
+		return list;
+	}
+
+	public static List<EventVO> eventSearchList(HashMap map){
+		SqlSession session = ssf.openSession();
+		List<EventVO> list= session.selectList("eventSearchList", map);
+		categorySet(list);
 		session.close();
 		return list;
 	}
@@ -129,7 +129,9 @@ public class EventDAO {
 		return vo;
 	}
 
+
 	/*
+	가격 정규화
 	public static void test(){
 		SqlSession session = ssf.openSession();
 		List<EventVO> list= session.selectList("replace_data");
