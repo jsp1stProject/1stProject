72b69886ab5e017b4703e755c5e1f91f4ed74db5
hare211 <blackhole2748@gmail.com>
2025-03-27 15:50:37 +0900

    김기현 - 리뷰 VO 수정

--- src/main/java/com/sist/vo/ReviewVO.java
+++ src/main/java/com/sist/vo/ReviewVO.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 @Getter @Setter
 public class ReviewVO {
-	private int no, content_id, rate;
-	private String type, user_id, message, dbday;
+	private int no, content_id, rate, type;
+	private String user_id, message, dbday;
 	private Date regdate;
 }
